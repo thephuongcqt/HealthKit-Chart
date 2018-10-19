53a56f230c27d96182b08b174be1e21f8b2762f3
Nguyen The Phuong <phuong.nt2417@gnt.com.vn>
2018-10-19 13:31:44 +0700

    Initial Commit

---
+++ HealthKit-Chart/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HealthKit-Chart
+//
+//  Created by Nguyen The Phuong on 10/19/18.
+//  Copyright © 2018 Nguyen The Phuong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
